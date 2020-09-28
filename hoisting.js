5ddf45322eb0064859cf196384d64adfe66b1c68
capable-optimization-2034 <brianna.l.castor@gmail.com>
2020-09-28 20:58:29 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -12,7 +12,7 @@ function callMe() {
 
 function crazy() {
     // fix the code in here
-    thisIsCrazy();
+    thisIsCrazy("hey!!!");
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
